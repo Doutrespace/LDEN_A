8eb9210ea459e0ef3763d6261cdd64a2000dddf5
Doutrespace <nils.karges@stud-mail.uni-wuerzburg.de>
2020-10-02 13:52:14 +0200

    changes

---
+++ Code.R
@@ -0,0 +1,89 @@
+#############################################################################################################
+############################################# Description ###################################################
+#############################################################################################################
+
+#############################################################################################################
+########################################## Load Libraries ###################################################
+#############################################################################################################
+# Create import function
+ipak <- function(pkg){
+  
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  
+  if (length(new.pkg)) 
+    
+    install.packages(new.pkg, dependencies = TRUE)
+  
+  sapply(pkg, require, character.only = TRUE)
+  
+}
+
+# Load libraries
+ipak(c("sp","sf","raster", "cleangeo"))
+#############################################################################################################
+########################################## Set WD datasets ##################################################
+#############################################################################################################
+Main_Dir <- "C:/Users/Nillus/OneDrive/Desktop/Noise_Pollution/DLR_Praktikum/Urban_NoiseR/Antonios_magicalfolder"
+setwd(Main_Dir)
+Data_dir <- paste0(Main_Dir,"/Data")
+Export_dir <- paste0(Main_Dir,"/Export")
+#############################################################################################################
+########################################## Load datasets ####################################################
+#############################################################################################################
+# Change to Data dir
+setwd(Data_dir)
+# Load Big polygon file (all europe)
+Noise_All <- st_read("name of big file")
+
+# Load Urban Atlas (this should be pulled from web based on name of city)
+UA <-st_read("Koblenz_UA_2012_pol.gpkg")
+
+AOI <- bbo
+#############################################################################################################
+#################################### Pre - Process datasets #################################################
+#############################################################################################################
+# 
+# Reproject UA
+UA <- st_transform(UA, crs = st_crs(25832))
+
+#############################################################################################################
+########################################## Process datasets #################################################
+#############################################################################################################
+
+#############################################################################################################
+################################################# Plots #####################################################
+#############################################################################################################
+
+
+Noise <- st_read("Koblenz_city_noise_fin.gpkg")
+
+plot(Noise)
+
+## RASTER
+# Function to create raster
+Raster_Bot <- reactive({
+  Koblenz_city_noise <- filteredData()
+  coordinates(Koblenz_city_noise) = ~LON+LAT
+  x.range <- as.double(range(Koblenz_city_noise@coords[,1]))
+  y.range <- as.double(range(Koblenz_city_noise@coords[,2]))
+  grd <- expand.grid(x=seq(from=x.range[1],
+                           to=x.range[2],
+                           by=input$res),
+                     y=seq(from=y.range[1],
+                           to=y.range[2],
+                           by=input$res))
+  coordinates(grd) <- ~ x+y
+  gridded(grd) <- TRUE
+  withProgress(message = 'Calculating raster', {
+    idw <- idw(formula=filteredData()[,selectVariable()] ~ 1, locations=Data2, newdata=grd)
+    idwrst <<- raster(idw)
+  })
+  idwrst <<- idwrst[[1]]
+  names(idwrst) <<- "TEMP" # variable name
+  crs(idwrst) <<- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+  
+  return(idwrst)
+  
+})
+
+############################################################################################################################
